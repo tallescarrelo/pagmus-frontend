1d4c5d1d7a7194e43e813e33c4d854a066fc9ec4
Talles Carrelo <tallescarrelo@gmail.com>
2025-07-26 23:13:07 -0300

    Correções: substituído imports de componentes removidos por conteúdo inline simples

--- src/pages/MaintenancePage.jsx
+++ src/pages/MaintenancePage.jsx
@@ -1,11 +1,16 @@
 import React from "react";
-import MaintenanceLayer from "../components/MaintenanceLayer";
 
 const MaintenancePage = () => {
   return (
-    <>
-      <MaintenanceLayer />
-    </>
+    <div className="d-flex align-items-center justify-content-center min-vh-100">
+      <div className="text-center">
+        <h1 className="display-4 mb-4">Manutenção</h1>
+        <p className="lead mb-4">Sistema em manutenção. Voltaremos em breve.</p>
+        <a href="/" className="btn btn-primary">
+          Voltar ao Login
+        </a>
+      </div>
+    </div>
   );
 };
 
