7485868b7cfdf69d7512b2696e22e1c42d8e749a
tallescarrelo <tallescarrelo@gmail.com>
2025-08-02 00:03:04 -0300

    feat: continuação da migração para TypeScript - páginas simples convertidas (AccessDeniedPage, ComingSoonPage, ErrorPage)

---
+++ src/pages/ComingSoonPage.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const ComingSoonPage: React.FC = () => {
+  return (
+    <div className="d-flex align-items-center justify-content-center min-vh-100">
+      <div className="text-center">
+        <h1 className="display-4 mb-4">Em Breve</h1>
+        <p className="lead mb-4">Esta funcionalidade estará disponível em breve.</p>
+        <a href="/" className="btn btn-primary">
+          Voltar ao Início
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default ComingSoonPage; 
---
+++ src/pages/ErrorPage.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import MasterLayout from "../masterLayout/MasterLayout";
+import Breadcrumb from "../components/Breadcrumb";
+import ErrorLayer from "../components/ErrorLayer";
+
+const ErrorPage: React.FC = () => {
+  return (
+    <>
+      {/* MasterLayout */}
+      <MasterLayout>
+
+        {/* Breadcrumb */}
+        <Breadcrumb title="404" />
+
+        {/* ErrorLayer */}
+        <ErrorLayer />
+
+      </MasterLayout>
+    </>
+  );
+};
+
+export default ErrorPage; 
